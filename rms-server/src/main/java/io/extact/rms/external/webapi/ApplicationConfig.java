1f6a3c46ee327a5782c522652aaec26a0e03c773
ozytso <ozytso@gmail.com>
2022-02-20 14:54:04 +0900

    release v1.0.0

--- rms-server/src/main/java/io/extact/rms/external/webapi/ApplicationConfig.java
+++ rms-server/src/main/java/io/extact/rms/external/webapi/ApplicationConfig.java
@@ -40,7 +40,7 @@ import io.extact.rms.platform.role.RoleSecurityDynamicFeature;
         bearerFormat = "JWT",
         in = SecuritySchemeIn.HEADER)
 @OpenAPIDefinition(
-        info = @Info(title = "レンタル予約システムの公開API", version = "1.0.0-SNAPSHOT",
+        info = @Info(title = "レンタル予約システムの公開API", version = "1.0.0",
             contact = @Contact(name = "課外活動", url = "https://extact-io.github.io/rms-website/")),
         components = @Components(
                 schemas = {
